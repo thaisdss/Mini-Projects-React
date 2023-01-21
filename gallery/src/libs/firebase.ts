a42129ef3d93f77f291b268d16512147d08a110b
Thaís Silva <thaisdss.silva@gmail.com>
2023-01-21 04:49:32 -0300

    update firebase.ts

--- gallery/src/libs/firebase.ts
+++ gallery/src/libs/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    storageBucket: 'd5reactgallery-6c33e.appspot.com',
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
     appId: process.env.REACT_APP_FIREBASE_APPID
 };
